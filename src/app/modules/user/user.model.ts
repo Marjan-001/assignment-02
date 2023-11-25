069e8e946b47924d6e0e636e4faf465752445a13
marjan <marjan199721@gmail.com>
2023-11-25 22:28:45 +0600

    paasword hashing

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,57 +1,71 @@
-import {  Schema,model } from "mongoose";
-import { TAddress, TFullName, TOrders, TUser } from "./user.interface";
-
-
+import { Schema, model } from 'mongoose'
+import { TAddress, TFullName, TOrders, TUser } from './user.interface'
+import bcrypt from 'bcrypt'
+import config from '../../config/config'
 
 const FullNameSchema = new Schema<TFullName>({
-
-firstName:{
-    type:String,
-    required:true,
-},
-lastName:{
-    type:String,
-    required:true,
-}
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
 })
 
-const AddressSchema= new Schema<TAddress>({
-    street:{
-        type:String,
-        required:true
-    },
-    city:{
-        type:String,
-        required:true,
-    },
-    country:{
-        type:String,
-        required:true, 
-    }
+const AddressSchema = new Schema<TAddress>({
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
 })
-const OrdersSchema = new Schema <TOrders>({
-    productName: String,
-    price: Number,
-    quantity: Number  
+const OrdersSchema = new Schema<TOrders>({
+  productName: String,
+  price: Number,
+  quantity: Number,
 })
 const UserSchema = new Schema<TUser>({
-    userId:{type:Number, required:true,unique:true},
-    username: {type:String, required:true},
-    password: {type:String},
-    fullName: {type:FullNameSchema,required:true,_id:false},
-    age: {type:Number},
-    email: {type:String,required:true,unique:true},
-   isActive:{type:Boolean},
-   hobbies:{type:[String]},
-   address:{type:AddressSchema,required:true,_id:false},
-   orders:{type:OrdersSchema}
+  userId: { type: Number, required: true, unique: true },
+  username: { type: String, required: true },
+  password: { type: String },
+  fullName: { type: FullNameSchema, required: true, _id: false },
+  age: { type: Number },
+  email: { type: String, required: true, unique: true },
+  isActive: { type: Boolean },
+  hobbies: { type: [String] },
+  address: { type: AddressSchema, required: true, _id: false },
+  orders: { type: OrdersSchema },
 })
 
+//hashing password using bcrypt
+ UserSchema.pre('save',async function(next){
+    const user =this;
+    user.password = await bcrypt.hash(
+        user.password,
+        Number(config. bcrypt_salt_rounds)
+    
+
+    )
+    next();
+ })
+
+
+
 //hide password from the api response
-UserSchema.methods.toJSON = function() {
-    const odject = this.toObject();
-  
-    delete odject.password;
-    return odject;
-   }
-export const User = model<TUser>('User',UserSchema)
+UserSchema.methods.toJSON = function () {
+  const odject = this.toObject()
+
+  delete odject.password;
+
+  return odject
+}
+export const User = model<TUser>('User', UserSchema)
